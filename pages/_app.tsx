35f11f841b4ac2ff989034d5f6dcb11771dd322c
Abdulhadi Web-Developer <aaaabbdulhadi573@gmail.com>
2026-03-13 12:22:45 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AdminAuthProvider } from '@/contexts/AdminAuthContext'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Check if it's an admin page
+  const isAdminPage = typeof window !== 'undefined' 
+    ? window.location.pathname.startsWith('/admin')
+    : false;
+
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <AdminAuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </AdminAuthProvider>
   )
 }
